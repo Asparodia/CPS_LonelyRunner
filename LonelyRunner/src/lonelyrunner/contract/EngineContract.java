9b3cc92ae012a54121a2c879991d9bfad39c971a
Asparodia <thiernobah91860@yahoo.fr>
2019-05-02 17:50:50 +0200

    nique sa grand mere les gardes

--- LonelyRunner/src/lonelyrunner/contract/EngineContract.java
+++ LonelyRunner/src/lonelyrunner/contract/EngineContract.java
@@ -1,12 +1,15 @@
 package lonelyrunner.contract;
 
 import java.util.ArrayList;
+
+import lonelyrunner.contract.contracterr.PreconditionError;
 import lonelyrunner.decorators.EngineDecorator;
 import lonelyrunner.service.EditableScreenService;
 import lonelyrunner.service.EngineService;
 import lonelyrunner.service.EnvironmentService;
 import lonelyrunner.service.GuardService;
 import lonelyrunner.service.PlayerService;
+import lonelyrunner.service.utils.Cell;
 import lonelyrunner.service.utils.Couple;
 import lonelyrunner.service.utils.Hole;
 import lonelyrunner.service.utils.Item;
@@ -17,6 +20,10 @@ public class EngineContract extends EngineDecorator{
 
 	public EngineContract(EngineService delegate) {
 		super(delegate);
+	}
+	
+	public void checkInvariant() {
+		
 	}
 
 	@Override
@@ -57,6 +64,36 @@ public class EngineContract extends EngineDecorator{
 	@Override
 	public void init(EditableScreenService es, Couple<Integer, Integer> posChar,
 			ArrayList<Couple<Integer, Integer>> posGuards, ArrayList<Couple<Integer, Integer>> posItems) {
+		
+		//\pre init(ES,posChar,posGuards,posItems) \req EditableScreenService::isPlayable(ES) == True 
+				//\and getCellNature(ES,posChar.x,posChar.y) == EMP
+				//\and \forall pos:Couple<Int,Int> in posGuards ((ScreenService::getCellNature(ES,pos.x,pos.y) = EMP) \and (pos.x != posChar.x \and pos.y != posChar.y))
+				//\and \forall posI:Couple<Int,Int> in posItems ((ScreenService::getCellNature(ES,posI.x,posI.y) = EMP) \and ((posI.x != posChar.x \and posI.y != posChar.y \and ( \forall pos:Couple<Int,Int> in posGuards posI.x!= pos.x \and  posI.y!=pos.y ))) )
+					//\and getCellNature(ES,posI.x,posI.y-1) == {PLT,MTL}
+		
+		// Pre
+		if(!es.isPlayable()) {
+			throw new PreconditionError("init()" , "editable screen not playable");
+		}
+		if(!(es.getCellNature(posChar.getElem1(), posChar.getElem2()) == Cell.EMP)){
+			throw new PreconditionError("init()" , "getCellNature(ES,posChar.x,posChar.y) == EMP");
+		}
+		for (Couple<Integer,Integer> c : posGuards) {
+			if(!(es.getCellNature(c.getElem1(), c.getElem2()) == Cell.EMP && c.getElem1() != posChar.getElem1() && c.getElem2() != posChar.getElem2())){
+				throw new PreconditionError("init()" , "\\forall pos:Couple<Int,Int> in posGuards ((ScreenService::getCellNature(ES,pos.x,pos.y) = EMP) \\and (pos.x != posChar.x \\and pos.y != posChar.y))");
+			}
+		}
+		for (Couple<Integer,Integer> c : posItems) {
+			for (Couple<Integer,Integer> cc : posGuards) {
+				if(!(c.getElem1() != cc.getElem1() && c.getElem2() != cc.getElem2())){
+					throw new PreconditionError("init()" , "\\forall pos:Couple<Int,Int> in posGuards posI.x!= pos.x \\and  posI.y!=pos.y ");
+				}
+			}
+			if(!(es.getCellNature(c.getElem1(), c.getElem2()-1) == Cell.EMP && es.getCellNature(c.getElem1(), c.getElem2()) == Cell.EMP && c.getElem1() != posChar.getElem1() && c.getElem2() != posChar.getElem2())){
+				throw new PreconditionError("init()" , "\\\forall posI:Couple<Int,Int> in posItems ((ScreenService::getCellNature(ES,posI.x,posI.y) = EMP) \\and ((posI.x != posChar.x \\and posI.y != posChar.y \\and ( \\forall pos:Couple<Int,Int> in posGuards posI.x!= pos.x \\and  posI.y!=pos.y ))) )\n" + 
+						"					//\\and getCellNature(ES,posI.x,posI.y-1) == {PLT,MTL}}");
+			}
+		}
 		getDelegate().init(es, posChar, posGuards, posItems);
 	}
 
